630c23620e5d52709a73c71da74adbbeb3d3821b
Vitali Savelyev <vsavelyev@elinext.com>
2022-06-25 17:13:53 +0300

    added error checking

--- src/index.ts
+++ src/index.ts
@@ -18,75 +18,89 @@ const wsServer = new WebSocketServer({
     port: Number(WSS_PORT) || 8080,
 })
 
+process.on('SIGINT', () => {
+    wsServer.close()
+    process.exit(0)
+})
+
 wsServer.on('connection', function connection(ws: WebSocket) {
     const duplex = createWebSocketStream(ws, { decodeStrings: false, encoding: 'utf8' });
     duplex.on('data', async (data: string) => {
-        const changedData: string[] = data.split(' ');
-        const action = changedData[0] ? changedData[0].split('_') : [];
-        const { x, y } = robot.getMousePos();
-        if (changedData[0] === 'mouse_position') {
-            console.log(`Recieved: mouse_position ${x},${y}`);
-        } else {
-            console.log(`Recieved: ${changedData[0]} ${changedData[1] || ''} ${changedData[2] || ''}`);
+        try {
+            const changedData: string[] = data.split(' ');
+            const action = changedData[0] ? changedData[0].split('_') : [];
+            const { x, y } = robot.getMousePos();
+            if (changedData[0] === 'mouse_position') {
+                console.log(`Recieved: mouse_position ${x},${y}`);
+            } else {
+                console.log(`Recieved: ${changedData[0]} ${changedData[1] || ''} ${changedData[2] || ''}`);
+            }
+            switch (action[0]) {
+                case 'mouse':
+                    const shift = Number(changedData[1]);
+                    switch (action[1]) {
+                        case 'position':
+                            duplex.write(`mouse_position ${x},${y} \0`)
+                            break;
+                        case 'up':
+                            robot.moveMouse(x, y - shift);
+                            duplex.write(`${changedData[0]}`)
+                            break;
+                        case 'down':
+                            robot.moveMouse(x, y + shift);
+                            duplex.write(`${changedData[0]}`)
+                            break;
+                        case 'left':
+                            robot.moveMouse(x - shift, y);
+                            duplex.write(`${changedData[0]}`)
+                            break;
+                        case 'right':
+                            robot.moveMouse(x + shift, y);
+                            duplex.write(`${changedData[0]}`)
+                            break;
+                    }
+                    break;
+                case 'draw':
+                    const size_1 = Number(changedData[1]);
+                    const size_2 = Number(changedData[2]);
+                    switch (action[1]) {
+                        case 'circle':
+                            drawCircle(size_1, x, y)
+                            break;
+                        case 'square':
+                            drawSquare(size_1, x, y)
+                            break;
+                        case 'rectangle':
+                            drawRectangle(size_1, size_2, x, y)
+                            break;
+                    }
+                    duplex.write(`${changedData[0]}`)
+                    break;
+                case 'prnt':
+                    const side: number = 200;
+                    await screen(side, x, y)
+                        .then((image: string) => {
+                            duplex.write(`prnt_scrn ${image} \0`);
+                        })
+                        .catch((error) => {
+                            console.log(`Error: ${error}`);
+                        });
+                    break;
+            }
         }
-        switch (action[0]) {
-            case 'mouse':
-                const shift = Number(changedData[1]);
-                switch (action[1]) {
-                    case 'position':
-                        duplex.write(`mouse_position ${x},${y} \0`)
-                        break;
-                    case 'up':
-                        robot.moveMouse(x, y - shift);
-                        duplex.write(`${changedData[0]}`)
-                        break;
-                    case 'down':
-                        robot.moveMouse(x, y + shift);
-                        duplex.write(`${changedData[0]}`)
-                        break;
-                    case 'left':
-                        robot.moveMouse(x - shift, y);
-                        duplex.write(`${changedData[0]}`)
-                        break;
-                    case 'right':
-                        robot.moveMouse(x + shift, y);
-                        duplex.write(`${changedData[0]}`)
-                        break;
-                }
-                break;
-            case 'draw':
-                const size_1 = Number(changedData[1]);
-                const size_2 = Number(changedData[2]);
-                switch (action[1]) {
-                    case 'circle':
-                        drawCircle(size_1, x, y)
-                        break;
-                    case 'square':
-                        drawSquare(size_1, x, y)
-                        break;
-                    case 'rectangle':
-                        drawRectangle(size_1, size_2, x, y)
-                        break;
-                }
-                duplex.write(`${changedData[0]}`)
-                break;
-            case 'prnt':
-                const side: number = 200;
-                await screen(side, x, y)
-                    .then((image: string) => {
-                        duplex.write(`prnt_scrn ${image} \0`);
-                    })
-                    .catch((error) => {
-                        console.log(`Error: ${error}`);
-                    });
-                break;
+        catch (err) {
+            console.log('operation failed')
         }
     });
     duplex.on("error", (err: Error) => {
-        console.log(err)
+        console.log(1, err)
     })
     duplex.write('something');
 });
+
+wsServer.on('error', (err) => {
+    console.log('websocket failed')
+})
 
 console.log(`Start static http server on the ${HTTP_PORT || 3000} port!`);
 httpServer.listen(HTTP_PORT || 3000);
